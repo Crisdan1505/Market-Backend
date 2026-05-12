4e04dadab27a0dcad01ff77aacb3ccbb9743cf34
Crisdan1505 <cristopher.euan@tecdesoftware.edu.mx>
2026-05-12 08:29:49 -0600

    test: adding HelloWorldController to test Spring

--- src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_a/HelloWorldController.java
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_a/HelloWorldController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
     public class HelloWorldController{
 
         @GetMapping("/hola")
-        public String saludar(){
+        public String saludar() {
             return "Hello World";
         }
-        
     }
